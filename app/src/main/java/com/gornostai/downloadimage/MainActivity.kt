c4911c83da14f525194b705574a981b9ea8d8535
evg-gornostai <harnastai750@gmail.com>
2022-04-07 14:46:02 +0300

    feature/download-image: -added the feature to download images

--- app/src/main/java/com/gornostai/downloadimage/MainActivity.kt
+++ app/src/main/java/com/gornostai/downloadimage/MainActivity.kt
@@ -2,10 +2,54 @@ package com.gornostai.downloadimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.gornostai.downloadimage.databinding.ActivityMainBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDownload.setOnClickListener { download() }
+        binding.btnClear.setOnClickListener { clear() }
+
+    }
+
+    fun download(){
+        if (binding.edLink.text.isNotEmpty()){
+            binding.progress.visibility = View.VISIBLE
+            Picasso.get()
+                .load(binding.edLink.text.toString())
+                .into(binding.ivImage, object: Callback{
+                    override fun onSuccess() {
+                        binding.progress.visibility = View.GONE
+                    }
+
+                    override fun onError(e: Exception?) {
+                        binding.progress.visibility = View.GONE
+                        makeToast()
+                    }
+                })
+        } else {
+            makeToast()
+        }
+
+    }
+
+    fun clear(){
+        binding.edLink.text.clear()
+        binding.ivImage.setImageDrawable(null)
+    }
+
+    fun makeToast(){
+        Toast.makeText(this@MainActivity,R.string.toast_msg,Toast.LENGTH_SHORT).show()
     }
 }
